0e904807354b8a651b4a32c5251fe078550677e4
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-22 10:52:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,9 @@ def self.all #accesses @@all
   @@all 
 end 
 
-def save # pushes new songs into class variable/empty array of songs
-  @@all << self 
-end 
+#def save ## pushes new songs into class variable/empty array of songs
+ # @@all << self 
+#end 
 
 def artist_name
 @@all.select{|song| song.artist.name == self}
