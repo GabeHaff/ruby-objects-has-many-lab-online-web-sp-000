753e0fb4743b27ae1e8897dfbeca940c15ca158b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-22 11:41:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -23,9 +23,9 @@ class Author
   
   def add_post(post)
     #def add_song(song)
-@songs << song 
-song.artist = self 
-@@song_count += 1 
-end 
+#@songs << song 
+#song.artist = self 
+#@@song_count += 1 
+#end 
   
 end 
