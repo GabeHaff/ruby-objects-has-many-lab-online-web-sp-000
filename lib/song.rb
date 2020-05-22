a2b445d814d451ba329b6c455f8a2b72ea16c18f
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-22 09:35:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song 
-  
+  @@all = []
   attr_accessor :name, :genre, :artist 
 def initialize(name)
   @name = name 
+  @@all << self.new.all 
 end 
 
-
-
+self.all 
+return @@all 
+end 
 end #class end 
